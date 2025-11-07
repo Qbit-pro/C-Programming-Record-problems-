3d5b688e65ffd5b8607961f08595c93fbc5004d1
Mohammed Irfan R <mohammedirfanr26@gmail.com>
2025-11-07 21:04:13 +0530

    Rename Program6.c to Program6A.c

--- Program6A.c
+++ Program6A.c
@@ -13,21 +13,3 @@ printf("\n a >= b = %d", a >= b); // Output is 1 if a is greater than or equal t
 printf("\n a <= b = %d", a <= b); // Output is 1 if a is less than or equal to b else 0
 getch();
 }
-
-// Program 2
-// #include <stdio.h>
-// #include <conio.h>
-// void main()
-// {
-// int a = 16, b = 0;
-// printf(" \na = %d b = %d", a, b);
-// // The output is 1 if a and b are non zerovalues, 0 otherwise
-// printf("\n a&&b = %d", a && b);
-// //The output is 1 if any one of a or b is non zero, 0 if both are zeros
-// printf("\n a||b = %d", a || b);
-// //The output is 1 if a is zero , 0 if a is non zero
-// printf("\n !a = %d", !a);
-// //The output is 1 if b is zero , 0 if b is non zero
-// printf("\n !b = %d", !b);
-// getch();
-// }
