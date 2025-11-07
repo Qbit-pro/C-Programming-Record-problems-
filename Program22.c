375a90de8142354d1b40f8481b52999527350daf
Mohammed Irfan R <mohammedirfanr26@gmail.com>
2025-11-07 21:40:36 +0530

    Update and rename Program22A.c to Program22.c

---
+++ Program22.c
@@ -0,0 +1,53 @@
+//Program 1
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+void main()
+{
+int array[10];
+int n, i ,sum = 0;
+printf("\nEnter the size of the array = ");
+scanf("%d",&n);
+printf("Enter the elements of the array one by one\n");
+for (i =0 ; i < n ; i++)
+{
+scanf("%d",array+i);
+sum = sum + (*(array+i));
+}
+for (i =0 ; i < n ; i++)
+{
+printf("\n Array[%d] = %d \n Address of Array[%d] = %d\n",
+i,*(array+i),i,array+i);
+}
+printf("\n Sum of the array elements: %d \n",sum);
+getch();
+
+}
+
+
+//Program 2
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+void main()
+{
+int *array = NULL; //pointer to integer
+int n = 0; //Variable store number of block
+int i ,sum = 0;
+printf("\nEnter the number of block = ");
+scanf("%d",&n); //Get input for number of block
+array = (int *)malloc(n * sizeof(int));
+printf("Enter the elements of the array one by one\n");
+for (i =0 ; i < n ; i++)
+{
+scanf("%d",array+i);
+sum+= *(array+i);
+}
+for (i =0 ; i < n ; i++)
+{
+printf("\narray[%d] = %d\n", i,array[i]);
+}
+printf("\n Sum of the array elements: %d \n",sum);
+free(array); // free allocated memory
+getch();
+}
